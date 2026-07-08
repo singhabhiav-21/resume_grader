ea2230eb8cec6dcf153a594cdb7f5065756a1fac
Abhinav Singh <abhinav.singh9421@gmail.com>
2026-07-08 09:14:49 +0200

    Added- dependecies for endpoints

---
+++ app/dependencies.py
@@ -0,0 +1,19 @@
+import jwt
+import os
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(token: str = Depends(oauth2)):
+    try:
+        payload = jwt.decode(token, key=os.getenv('JWT_KEY'), algorithms=[os.getenv('JWT_ALG')])
+        user_id = payload.get("sub")
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return user_id
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Expired Token")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid Token")
