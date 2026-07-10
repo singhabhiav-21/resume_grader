4ab64437456102444b6a91890c493fbba9f66404
Abhinav Singh <abhinav.singh9421@gmail.com>
2026-07-10 09:52:29 +0200

    Added- jobs services

---
+++ app/services/job_to_user.py
@@ -0,0 +1,22 @@
+from starlette.exceptions import HTTPException
+from app.conn_session import db
+from app.models.job import Jobs
+
+
+def check_job(job_id, user_id):
+    with db() as session:
+        query = session.query(Jobs).filter(Jobs.job_id == job_id).first()
+        return query is not None and query.user_id == user_id
+
+
+def add_job(job_id, user_id, status):
+    with db() as session:
+        query = Jobs(job_id, user_id, status)
+        session.add(query)
+
+
+def update_job(job_id, user_id, status):
+    with db() as session:
+        query = session.query(Jobs).filter(Jobs.job_id == job_id).first()
+        if query:
+            query.status = status
