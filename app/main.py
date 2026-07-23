1bc909d50f724e2c293a27e53b2e82998c1b448d
Abhinav Singh <abhinav.singh9421@gmail.com>
2026-07-24 00:57:08 +0200

    Added docker fiile for backend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, status, HTTPException, Depends
-import app.routers.auth as auth
+from app.routers import auth, analyze
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(auth.router)
 
+app.include_router(analyze.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def user(user: None):
