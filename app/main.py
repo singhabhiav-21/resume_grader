cc800481c1e293525fb91da1f6a5d5945b41d8e8
Abhinav Singh <abhinav.singh9421@gmail.com>
2026-07-28 23:44:53 +0200

    Updated main for cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status, HTTPException, Depends
 from app.routers import auth, analyze
 from fastapi.middleware.cors import CORSMiddleware
+import os
 app = FastAPI()
 app.include_router(auth.router)
 
@@ -8,10 +9,11 @@ app.include_router(analyze.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    allow_origins=[os.getenv("FRONTEND_URL", "http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
+
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def user(user: None):
